3b361dfa1f448aca68d63daf45a0e4827092b846
bamaunocerocuatro <bamaunocerocuatro@gmail.com>
2026-03-23 20:00:49 -0300

    fix firebase-admin exports

--- app/lib/firebase-admin.ts
+++ app/lib/firebase-admin.ts
@@ -6,6 +6,11 @@ if (!admin.apps.length) {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
+    } as admin.ServiceAccount),
   });
 }
+
+const db = admin.firestore();
+const adminAuth = admin.auth();
+
+export { db, adminAuth };
