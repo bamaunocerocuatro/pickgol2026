5284c8ef0c629f67865ae081d0ef82a5ac5f266d
bamaunocerocuatro <bamaunocerocuatro@gmail.com>
2026-04-02 21:56:07 -0300

    agregar pagina de privacidad

---
+++ app/privacy/page.tsx
@@ -0,0 +1,51 @@
+ export default function Privacy() {
+  return (
+    <main style={{fontFamily:'Helvetica Neue, Helvetica, Arial, sans-serif', padding:'1em', maxWidth:'800px', margin:'0 auto'}}>
+      <strong>Privacy Policy</strong>
+      <p>This privacy policy applies to the PICKGOL app (hereby referred to as "Application") for mobile devices that was created by Ari Percal (hereby referred to as "Service Provider") as a Commercial service. This service is intended for use "AS IS".</p>
+      
+      <strong>Information Collection and Use</strong>
+      <p>The Application collects information when you download and use it. This information may include:</p>
+      <ul>
+        <li>Your device's Internet Protocol address (e.g. IP address)</li>
+        <li>The pages of the Application that you visit, the time and date of your visit, the time spent on those pages</li>
+        <li>The time spent on the Application</li>
+        <li>The operating system you use on your mobile device</li>
+      </ul>
+      <p>The Application does not gather precise information about the location of your mobile device.</p>
+      <p>The Application does not use Artificial Intelligence (AI) technologies to process your data or provide features.</p>
+      <p>The Service Provider may use the information you provided to contact you from time to time to provide you with important information, required notices and marketing promotions.</p>
+      <p>For a better experience, while using the Application, the Service Provider may require you to provide certain personally identifiable information. The information that the Service Provider requests will be retained by them and used as described in this privacy policy.</p>
+
+      <strong>Third Party Access</strong>
+      <p>Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Application utilizes third-party services that have their own Privacy Policy about handling data:</p>
+      <ul>
+        <li><a href="https://www.google.com/policies/privacy/">Google Play Services</a></li>
+        <li><a href="https://firebase.google.com/support/privacy">Google Analytics for Firebase</a></li>
+        <li><a href="https://firebase.google.com/support/privacy/">Firebase Crashlytics</a></li>
+      </ul>
+
+      <strong>Opt-Out Rights</strong>
+      <p>You can stop all collection of information by the Application easily by uninstalling it.</p>
+
+      <strong>Data Retention Policy</strong>
+      <p>The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. To request deletion, contact: <a href="mailto:billing@pickgol.com">billing@pickgol.com</a></p>
+
+      <strong>Children</strong>
+      <p>The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13. If you are a parent or guardian and you are aware that your child has provided personal information, please contact <a href="mailto:billing@pickgol.com">billing@pickgol.com</a>.</p>
+
+      <strong>Security</strong>
+      <p>The Service Provider is concerned about safeguarding the confidentiality of your information and provides physical, electronic, and procedural safeguards to protect information processed and maintained.</p>
+
+      <strong>Changes</strong>
+      <p>This Privacy Policy may be updated from time to time. The Service Provider will notify you of any changes by updating this page. This privacy policy is effective as of 2026-04-03.</p>
+
+      <strong>Your Consent</strong>
+      <p>By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy.</p>
+
+      <strong>Contact Us</strong>
+      <p>If you have any questions regarding privacy, please contact: <a href="mailto:billing@pickgol.com">billing@pickgol.com</a></p>
+    </main>
+  );
+}
+
