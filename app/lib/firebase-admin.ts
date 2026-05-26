cb7b444e15c0e179d936e1fef4cbc9f7b6f80dec
bamaunocerocuatro <bamaunocerocuatro@gmail.com>
2026-05-26 00:10:34 -0300

    debug: firebase admin logs

--- app/lib/firebase-admin.ts
+++ app/lib/firebase-admin.ts
@@ -1,11 +1,19 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+  console.log('Firebase Admin init:', {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKeyStart: privateKey?.substring(0, 50),
+    privateKeyEnd: privateKey?.substring(privateKey.length - 50),
+  });
+  
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey,
     } as admin.ServiceAccount),
   });
 }
